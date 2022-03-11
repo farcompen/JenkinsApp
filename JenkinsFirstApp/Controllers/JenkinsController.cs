05da0cf0d0e40cc286ad9a68ce00c57ec2cde5be
cankirism <cankiri.bilsis@saglik.gov.tr>
2022-03-11 14:19:03 +0300

    Add project files.

---
+++ JenkinsFirstApp/Controllers/JenkinsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JenkinsFirstApp.Controllers
+{
+    [Route("api/v1/")]
+    [ApiController]
+    public class JenkinsController : ControllerBase
+    {
+
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Up" });
+        
+        }
+    }
+}
